e9c301f43c479dac5ccc0b0452025881d9f8b6d4
Harold Calderon <harold.1408@hotmail.com>
2021-09-08 09:20:05 -0500

    En el archivo main.js se coloca movimiento de las teclas

--- main.js
+++ main.js
@@ -184,4 +184,52 @@ get elements(){
    }
   }
  })();
- 
+ var board = new Board(800,400);
+ var bar = new Bar(20,100,40,100,board);
+ var bar_2 = new Bar(735,100,40,100,board);
+ var canvas = document.getElementById('canvas');
+ var board_view = new BoardView(canvas,board);
+ var ball = new Ball(350,100,10,board);
+
+
+ document.addEventListener("keydown",function(ev){
+   //console.log(ev.keyCode);
+   if(ev.keyCode == 87){
+       ev.preventDefault();  
+    bar.up();
+    
+   }
+   else if(ev.keyCode == 65){
+    ev.preventDefault();
+    bar.down();
+   }
+   else if(ev.keyCode == 68){
+       ev.preventDefault();  
+    bar_2.up();
+   }
+   else if(ev.keyCode == 90){
+    ev.preventDefault();
+    bar_2.down();
+   }
+   else if (ev.keyCode ===32){
+    ev.preventDefault();
+    board.playing = !board.playing;
+  
+   }
+  
+  
+  });
+  
+
+  board_view.draw();
+  window.requestAnimationFrame(controller);
+  setTimeout(function(){
+    ball.direction = -2;
+  },4000);
+  
+  function controller(){
+    board_view.play(); 
+    window.requestAnimationFrame(controller);
+  
+  }
+  
